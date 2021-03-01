2ca9b889f38b67fe79d722ea6ff718e6bb2ac5a4
Ciro Cesar Maciel <me@ciro-maciel.me>
2021-03-01 12:36:07 -0300

    fix: updade dependency

--- src/rest.js
+++ src/rest.js
@@ -1,12 +1,10 @@
 const serverless = require("serverless-http");
-const serverlessExpress = require("@vendia/serverless-express");
+// const serverlessExpress = require("@vendia/serverless-express");
 
 const express = require("express");
 const bodyParser = require("body-parser");
 const helmet = require("helmet");
 const cors = require("cors");
-
-const { REST_PATH } = process.env;
 
 const app = express();
 
@@ -16,17 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  console.log("req", req);
-  console.log("REST_PATH", REST_PATH);
-  console.log("process.env", process.env);
-
-  const newUrl = req.url.replace("/s/p", "").replace("/s/t", "");
-
-  req.url = newUrl;
-  // req.originalUrl = newUrl;
-
-  console.log("req", req);
-
+  req.url = req.url.replace("/s/p", "").replace("/s/t", "");
   return next();
 });
 
@@ -34,5 +22,5 @@ app.get("/health", (req, res) => {
   res.status(200).send({ status: "ok", message: "👌 - Okay running" });
 });
 
-// module.exports.handler = serverless(app);
-module.exports.handler = serverlessExpress({ app });
+module.exports.handler = serverless(app);
+// module.exports.handler = serverlessExpress({ app });
