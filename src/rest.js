e3a1546e8f10bee1e802618bb88fb8afe33ca9ac
Ciro Cesar Maciel <me@ciro-maciel.me>
2021-03-01 12:10:12 -0300

    fix(router): path

--- src/rest.js
+++ src/rest.js
@@ -6,6 +6,8 @@ const bodyParser = require("body-parser");
 const helmet = require("helmet");
 const cors = require("cors");
 
+const { REST_PATH } = process.env;
+
 const app = express();
 
 app.use(cors());
@@ -13,28 +15,11 @@ app.use(helmet());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use((ctx, next) => {
+app.use((ctx, x, next) => {
   console.log("ctx", ctx);
 
-  ctx.lambdaEvent =
-    (ctx.headers["x-apigateway-event"] &&
-      JSON.parse(decodeURIComponent(ctx.headers["x-apigateway-event"]))) ||
-    {};
-  ctx.lambdaContext =
-    (ctx.headers["x-apigateway-context"] &&
-      JSON.parse(decodeURIComponent(ctx.headers["x-apigateway-context"]))) ||
-    {};
-  ctx.env = (ctx.lambdaEvent && ctx.lambdaEvent.stageVariables) || process.env;
+  ctx.url = ctx.url.replace(REST_PATH, "");
 
-  // Workaround an inconsistency in APIG. For custom domains, it puts the
-  // mapping prefix on the url, but non-custom domain requests do not. Fix it by
-  // changing the path to the proxy param which has the correct value always.
-  if (ctx.lambdaEvent.pathParameters && ctx.lambdaEvent.pathParameters.proxy) {
-    const dummyBase = "zz://zz";
-    const url = new URL(ctx.url, dummyBase);
-    url.pathname = "/" + ctx.lambdaEvent.pathParameters.proxy;
-    ctx.url = url.href.replace(dummyBase, "");
-  }
   return next();
 });
 
