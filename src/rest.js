61b25cd2d7716da04e0247fcb1b9dad7d86ade29
Ciro Cesar Maciel <me@ciro-maciel.me>
2021-03-01 14:55:49 -0300

    fix(path): base

--- src/rest.js
+++ src/rest.js
@@ -7,17 +7,12 @@ const app = new Koa();
 
 const { SECRET } = process.env;
 
-app.use(jwt({ secret: SECRET }).unless({ path: [/^\/health/, /^\//] }));
-
 app.use(async (ctx, next) => {
   ctx.url = ctx.url.replace("/s/p", "").replace("/s/t", "");
   return next();
 });
 
-app.use(async (ctx, next) => {
-  ctx.url = ctx.url.replace("/s/p", "").replace("/s/t", "");
-  return next();
-});
+app.use(jwt({ secret: SECRET }).unless({ path: ["/", /^\/health/] }));
 
 app.use(async (ctx) => {
   if (ctx.path === "/") {
